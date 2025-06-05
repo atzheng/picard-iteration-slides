45e11dd5413381088f62a81f51a63728fd0f9822
Andrew Zheng <azheng92@gmail.com>
2025-06-05 14:07:33 -0500

    Updates

--- PicardBuild.py
+++ PicardBuild.py
@@ -2,13 +2,76 @@ from manim_templates import *
 from manim import *
 import funcy as f
 
+from SequentialBuild import create_sequence
+
 class PicardBuild(BaseSlide):
     def construct(self):
-        self.setup_slide(title="PicardBuild")
+        self.setup_slide(title="Picard Iteration")
 
+        T = 4
+        # Create two sequences
+        rs1, pis1, acs1, fs1, oms1, arrows1 = create_sequence(T)
+        rs2, pis2, acs2, fs2, oms2, arrows2 = create_sequence(T)
 
+        acache = [
+            f.lmap(MathTex, [f"a_{i}^{{({j})}}" for i in range(T)])
+            for j in range(2)
+        ]
 
-        self.play_animations(
-            [
-            ]
+        # Create groups for each sequence
+        seq1 = VGroup(*rs1, *pis1, *acs1, *fs1, *oms1, *arrows1.get_all_submobjects())
+        seq2 = VGroup(*rs2, *pis2, *acs2, *fs2, *oms2, *arrows2.get_all_submobjects())
+
+        # Arrange sequences side by side
+        seq1.shift(LEFT * 3.5)
+        seq2.shift(RIGHT * 3.5)
+
+        # Create animations for both sequences
+        anims = []
+        for i in range(T):
+            # First sequence
+            state_group1 = [rs1[i]]
+            if i > 0:
+                state_group1.append(arrows1[(fs1[i - 1], rs1[i])])
+            anims.append(AnimationGroup(*[FadeIn(obj) for obj in state_group1]))
+            
+            if i not in [1, 3]:  # Only animate policies for indices 0, 2
+                anims.append(AnimationGroup(FadeIn(arrows1[(rs1[i], pis1[i])]), FadeIn(pis1[i])))
+                anims.append(AnimationGroup(FadeIn(arrows1[(pis1[i], acs1[i])]), FadeIn(acs1[i])))
+            else:
+                anims.append(FadeIn(acs1[i]))
+
+            anims.append(
+                AnimationGroup(
+                    FadeIn(arrows1[(acs1[i], fs1[i])]),
+                    FadeIn(fs1[i]),
+                    FadeIn(arrows1[(rs1[i], fs1[i])]),
+                    FadeIn(arrows1[(fs1[i], oms1[i])]),
+                    FadeIn(oms1[i]),
+                )
+            )
+
+            # Second sequence
+            state_group2 = [rs2[i]]
+            if i > 0:
+                state_group2.append(arrows2[(fs2[i - 1], rs2[i])])
+            anims.append(AnimationGroup(*[FadeIn(obj) for obj in state_group2]))
+            
+            if i not in [0, 2]:  # Only animate policies for indices 1, 3
+                anims.append(AnimationGroup(FadeIn(arrows2[(rs2[i], pis2[i])]), FadeIn(pis2[i])))
+                anims.append(AnimationGroup(FadeIn(arrows2[(pis2[i], acs2[i])]), FadeIn(acs2[i])))
+            else:
+                anims.append(FadeIn(acs2[i]))
+
+            anims.append(
+                AnimationGroup(
+                    FadeIn(arrows2[(acs2[i], fs2[i])]),
+                    FadeIn(fs2[i]),
+                    FadeIn(arrows2[(rs2[i], fs2[i])]),
+                    FadeIn(arrows2[(fs2[i], oms2[i])]),
+                    FadeIn(oms2[i]),
+                )
+            )
+
+        self.play_animations(anims
         )
--- SequentialBuild.py
+++ SequentialBuild.py
@@ -4,86 +4,128 @@ import funcy as f
 import itertools as it
 
 
+def make_arrow(start, end, **kwargs):
+    default_kwargs = {
+        "buff": 0.1,
+        "stroke_width": 1,
+        "max_tip_length_to_length_ratio": 0.15,
+    }
+    return Arrow(start, end, **{**default_kwargs, **kwargs})
+
+
+def create_sequence(T):
+    seq = VGroup(
+        *it.chain.from_iterable(
+            [
+                f.lmap(
+                    MathTex,
+                    [rf"s_{i}", rf"\pi", rf"a_{i}", r"f", rf"\omega_{i}"],
+                )
+                for i in range(T)
+            ]
+        )
+    ).arrange_in_grid(n_rows=T, n_cols=5, buff=1)
+
+    rs, pis, acs, fs, oms = [
+        [seq[5 * i + j] for i in range(T)] for j in range(5)
+    ]
+
+    for pi in pis:
+        pi.add_background_rectangle(color=RED, buff=0.2)
+
+    for fx in fs:
+        fx.add_background_rectangle(color=GREEN, buff=0.2)
+
+    arrows = VDict(
+        it.chain.from_iterable(
+            [
+                [
+                    ((s, pi), make_arrow(s.get_right(), pi.get_left()))
+                    for s, pi in zip(rs, pis)
+                ],
+                [
+                    ((pi, ac), make_arrow(pi.get_right(), ac.get_left()))
+                    for pi, ac in zip(pis, acs)
+                ],
+                [
+                    ((ac, f), make_arrow(ac.get_right(), f.get_left()))
+                    for ac, f in zip(acs, fs)
+                ],
+                [
+                    ((f, om), make_arrow(om.get_left(), f.get_right()))
+                    for f, om in zip(fs, oms)
+                ],
+                [
+                    (
+                        (f, s_next),
+                        make_arrow(
+                            f.get_bottom(),
+                            s_next.get_top(),
+                            max_tip_length_to_length_ratio=0.05,
+                        ),
+                    )
+                    for f, s_next in zip(fs[:-1], rs[1:])
+                ],
+                [
+                    (
+                        (s, f),
+                        CurvedArrow(
+                            s.get_bottom(),
+                            f.get_left(),
+                            angle=1,
+                            color=BLACK,
+                            stroke_width=1,
+                            tip_length=0.15,
+                        ),
+                    )
+                    for s, f in zip(rs, fs)
+                ],
+            ]
+        )
+    )
+
+    rs.append(VGroup())
+    arrows[(fs[-1], rs[-1])] = VGroup()
+    
+    return rs, pis, acs, fs, oms, arrows
+
+
 class SequentialBuild(BaseSlide):
     def construct(self):
         self.setup_slide(title="Sequential Simulation ")
 
         T = 4
-
-        seq = VGroup(
-            *it.chain.from_iterable(
-                [
-                    f.lmap(
-                        MathTex,
-                        [rf"s_{i}", rf"\pi", rf"a_{i}", r"f", rf"\omega_{i}"],
-                    )
-                    for i in range(T)
-                ]
-            )
-        ).arrange_in_grid(n_rows=4, n_cols=5, buff=1)
-
-        rs, pis, acs, fs, oms = [
-            [seq[5 * i + j] for i in range(T)] for j in range(5)
-        ]
-
-        for pi in pis:
-            pi.add_background_rectangle(color=RED, buff=0.2)
-
-        for fx in fs:
-            fx.add_background_rectangle(color=GREEN, buff=0.2)
-
-        arrows = VDict(
-            it.chain.from_iterable([
-                [
-                    ((s, pi), Arrow(s.get_right(), pi.get_left(), buff=0.1))
-                    for s, pi in zip(rs, pis)
-                ],
-                [
-                    ((pi, ac), Arrow(pi.get_right(), ac.get_left(), buff=0.1))
-                    for pi, ac in zip(pis, acs)
-                ],
-                [
-                    ((ac, f), Arrow(ac.get_right(), f.get_left(), buff=0.1))
-                    for ac, f in zip(acs, fs)
-                ],
-                [
-                    ((f, om), Arrow(om.get_left(), f.get_right(), buff=0.1))
-                    for f, om in zip(fs, oms)
-                ],
-                [
-                    ((f, s_next), Arrow(f.get_bottom(), s_next.get_top(), buff=0.1))
-                    for f, s_next in zip(fs[:-1], rs[1:])
-                ]
-            ])
-        )
-
-        rs.append(VGroup())
-        arrows[(fs[-1], rs[-1])] = VGroup()
+        rs, pis, acs, fs, oms, arrows = create_sequence(T)
 
         anims = []
         for i in range(T):
-            anims.append(FadeIn(rs[i], run_time=0.5))
+            # State node and its incoming arrow (except first state)
+            state_group = [rs[i]]
+            if i > 0:
+                state_group.append(arrows[(fs[i - 1], rs[i])])
+            anims.append(AnimationGroup(*[FadeIn(obj) for obj in state_group]))
 
+            # Policy node and its incoming arrow
+            anims.append(
+                AnimationGroup(FadeIn(arrows[(rs[i], pis[i])]), FadeIn(pis[i]))
+            )
 
-        self.play_animations(
-            [
-                *map(
-                    FadeIn,
-                    it.chain.from_iterable([
-                        [rs[i],
-                         arrows[(rs[i], pis[i])],
-                         pis[i],
-                         arrows[(pis[i], acs[i])],
-                         acs[i],
-                         oms[i],
-                         arrows[(acs[i], fs[i])],
-                         arrows[(fs[i], oms[i])],
-                         fs[i],
-                         arrows[(fs[i], rs[i + 1])],
-                         ]
-                        for i in range(T)
-                    ]),
-                ),
-                # FadeIn(arrows),
-            ]
-        )
+            # Action node and its incoming arrow
+            anims.append(
+                AnimationGroup(FadeIn(arrows[(pis[i], acs[i])]), FadeIn(acs[i]))
+            )
+
+            # Transition function, outcome, and their arrows
+            anims.append(
+                AnimationGroup(
+                    FadeIn(arrows[(acs[i], fs[i])]),
+                    FadeIn(fs[i]),
+                    FadeIn(
+                        arrows[(rs[i], fs[i])]
+                    ),  # Add arc from state to transition
+                    FadeIn(arrows[(fs[i], oms[i])]),
+                    FadeIn(oms[i]),
+                )
+            )
+
+        self.play_animations(anims)
